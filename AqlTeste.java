d417f0c3018fbdd68a8fa7447eb54d56f907e20f
Guilherme Nacarelli <guilherme.pinheiro@germinare.org.br>
2024-03-19 16:51:52 -0300

    Com arquivo .java

---
+++ AqlTeste.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class AqlTeste {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nome;
+
+        System.out.println("Digite seu nome:");
+        nome = input.nextLine();
+
+        System.out.println("Seu nome é "+nome+" .");
+    }
+}
