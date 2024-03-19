db5c932fc1ed8a76cb5f3863c03e73d703c48241
Guilherme Nacarelli <guilherme.pinheiro@germinare.org.br>
2024-03-19 17:20:48 -0300

    Criando outro arquivo .java para dar o merge

---
+++ Agora_entendi.java
@@ -0,0 +1,8 @@
+public class Agora_entendi {
+    public static void main(String[] args) {
+        int contaBasica;
+        contaBasica = 301 + 77*4;
+
+        System.out.println(contaBasica+" ao quadrado");
+    }
+}
