ce35d2860b948031abd94776516645dab146fa19
Guilherme Nacarelli <guilherme.pinheiro@germinare.org.br>
2024-03-19 17:28:57 -0300

    Curinthia

--- AqlTeste.java
+++ AqlTeste.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class AqlTeste {
     public static void main(String[] args) {
+        int contaBasica;
+        contaBasica = 301 + 77*4;
+        
         Scanner input = new Scanner(System.in);
 
         String nome;
